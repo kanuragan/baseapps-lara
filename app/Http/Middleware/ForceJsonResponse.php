75d346b56cb6e33b3f26a65b5583d7ab432a1dc5
unknown <huda.alfarizi.it@gmail.com>
2021-05-07 22:07:42 +0700

    add validation client_id and client_secret for check apps access

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ForceJsonResponse
 {
@@ -17,6 +18,25 @@ class ForceJsonResponse
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
+        $client_id     = $request->client_id;
+        $client_secret = $request->client_secret;
+
+        $cek_client_apps = DB::table('oauth_clients')
+                                ->where([
+                                    'id'     => $client_id,
+                                    'secret' => $client_secret,
+                                ])
+                                ->count();
+        if($cek_client_apps != 1 ) {
+            return response()->json([
+                'response' => 'unauthorized',
+            ], 401);
+        }
+        if (! $request->expectsJson()) {
+            return response()->json([
+                'response' => 'unauthorized',
+            ], 401);
+        }
         return $next($request);
     }
 }
